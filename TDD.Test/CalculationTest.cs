18c6306f1ab273149ba972f52ee3a0dd80e9263d
AlisonSerra <alison.serra@hotmail.fr>
2023-05-14 23:04:05 +0200

    Base 1 - Test Red

---
+++ TDD.Test/CalculationTest.cs
@@ -0,0 +1,65 @@
+namespace TDD.Test
+{
+    public class CalculationTest
+    {
+        [Fact]
+        public void TestAddition()
+        {
+            // Arrange
+            int x = 2;
+            int y = 3;
+            int expectedSum = 5;
+
+            // Act
+            int actualSum = Calculator.Add(x, y);
+
+            // Assert
+            Assert.Equal(expectedSum, actualSum);
+        }
+
+        [Fact]
+        public void TestMultiplication()
+        {
+            // Arrange
+            int x = 5;
+            int y = 3;
+            int expectedProduct = 15;
+
+            // Act
+            int actualProduct = Calculator.Multiply(x, y);
+
+            // Assert
+            Assert.Equal(expectedProduct, actualProduct);
+        }
+
+        [Fact]
+        public void TestDivision()
+        {
+            // Arrange
+            int x = 10;
+            int y = 3;
+            int expectedDivision = 3;
+
+            // Act
+            int actualDivision = Calculator.Divide(x, y);
+
+            // Assert
+            Assert.Equal(expectedDivision, actualDivision);
+        }
+
+        [Fact]
+        public void TestSubtraction()
+        {
+            // Arrange
+            int x = 8;
+            int y = 3;
+            int expectedDifference = 5;
+
+            // Act
+            int actualDifference = Calculator.Subtract(x, y);
+
+            // Assert
+            Assert.Equal(expectedDifference, actualDifference);
+        }
+    }
+}
---
+++ TDD/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+
+namespace TDD
+{
+    public class Calculator
+    {
+        public static int Add(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int Multiply(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int Divide(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int Subtract(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

1607209f22af30c819236a582cec00c0528f6391
AlisonSerra <alison.serra@hotmail.fr>
2023-05-14 23:17:13 +0200

    Base 1 - Test Green

--- TDD/Calculator.cs
+++ TDD/Calculator.cs
@@ -6,22 +6,22 @@ namespace TDD
     {
         public static int Addition(int x, int y)
         {
-            throw new NotImplementedException();
+            return x + y;
         }
 
         public static int Multiply(int x, int y)
         {
-            throw new NotImplementedException();
+            return x * y;
         }
 
         public static int Divide(int x, int y)
         {
-            throw new NotImplementedException();
+            return x / y;
         }
 
         public static int Subtraction(int x, int y)
         {
-            throw new NotImplementedException();
+            return x - y;
         }
 
     }
